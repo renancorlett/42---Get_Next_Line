326e83f2e7ac4253dbe19179d9a7baa8662eff8d
Renan Corlett <renancorlett@gmail.com>
2024-11-12 10:03:39 +0000

    add extension .txt

--- Tests/main.c
+++ Tests/main.c
@@ -2,27 +2,28 @@
 #include <stdio.h>  // Para printf()
 #include "get_next_line.h"
 
-int	main(void)
+int main(void)
 {
-	int fd;
-	char *line;
+    int fd;
+    char *line;
 
-	// Abre o arquivo em modo leitura
-	fd = open("1char.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Erro ao abrir o arquivo");
-		return (1);
-	}
+    // Abre o arquivo em modo leitura
+    fd = open("1char.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Erro ao abrir o arquivo");
+        return (1);
+    }
 
-	// Lê e imprime cada linha
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s", line);
-		free(line);
-	}
+    // Lê e imprime cada linha
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
 
-	// Fecha o arquivo
-	close(fd);
-	return (0);
+    // Fecha o arquivo
+    close(fd);
+    return (0);
 }
+
